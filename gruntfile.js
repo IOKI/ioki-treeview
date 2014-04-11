3c3d8090f412a5b083644321de9e74e651114343
Tomasz Nieżurawski <tommaqs@gmail.com>
2014-04-11 14:44:28 +0200

    +Grunt: Livereload and Watch

--- gruntfile.js
+++ gruntfile.js
@@ -5,18 +5,52 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
             files: [
-                'demo/**/*.js'
+                'demo/**/*.js',
+                'gruntfile.js'
             ],
             options: {
                 jshintrc: '.jshintrc',
                 reporter: require('jshint-stylish')
             }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 9000,
+                    hostname: 'localhost',
+                    open: {
+                        target: 'http://localhost:9000/demo/development.html'
+                    },
+                    keepalive: true
+                }
+            }
+        },
+        watch: {
+            html: {
+                files: [
+                    'demo/**/*.html'
+                ],
+                options: {
+                    spawn: false,
+                    livereload: true
+                }
+            }
+        },
+        concurrent: {
+            server: [
+                'connect:server',
+                'watch'
+            ]
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-concurrent');
 
     grunt.registerTask('test', ['jshint']);
+    grunt.registerTask('serve', ['concurrent:server']);
     grunt.registerTask('build', ['jshint']);
 
     grunt.registerTask('default', ['build']);
